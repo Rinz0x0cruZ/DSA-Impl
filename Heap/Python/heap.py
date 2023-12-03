397d008dbe4f5e302cdd728ca5051d2a4b93d459
Rinz0x0cruZ <rinz0x0cruz@gmail.com>
2023-12-03 11:31:48 +0530

    Heap

---
+++ Heap/Python/heap.py
@@ -0,0 +1,12 @@
+class Heap:
+    def __init__(self) -> None:
+        pass
+
+    def insert(self,data):
+        pass
+    def delete(self,data):
+        pass
+    def bubbleup(self):
+        pass
+    def bubbledown(self):
+        pass
