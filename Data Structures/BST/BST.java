8ff381c4f0e1c3e93ca229007aa130e73b04fcbb
Rinz0x0cruZ <rinz0x0cruz@gmail.com>
2024-01-03 21:05:56 +0530

    BST

--- Data Structures/BST/BST.java
+++ Data Structures/BST/BST.java
@@ -1,35 +1,37 @@
 public class BST<T extends Comparable<T>> {
     private class Node{
         T data;
+        Integer idx;
         Node left,right;
         Node(){
             this.left=null;
             this.right=null;
         }
-        Node(T data){
+        Node(T data,Integer index){
             this.data=data;
+            this.idx=index;
         }
     }
     Node root;
     public BST(T[] arr){
         makeTree(arr);
     }
-    public void insert(T x){
+    public void insert(T x,Integer index){
         if(this.root==null){
-            this.root=new Node(x);
+            this.root=new Node(x,index);
             return ;
         }
         Node temp=this.root;
         while(true){
             if(x.compareTo(temp.data)>0){
                 if(temp.right==null){
-                    temp.right=new Node(x);
+                    temp.right=new Node(x,index);
                     break;
                 }
                 temp=temp.right;
             } else {
                 if(temp.left==null){ 
-                    temp.left=new Node(x);
+                    temp.left=new Node(x,index);
                     break;
                 }
                 temp=temp.left;
@@ -40,13 +42,12 @@ public class BST<T extends Comparable<T>> {
         Node temp=this.root;
         traverse(temp);
     }
-    public void update(T x){
+    public void update(T x,T f){
         Node temp=this.root;
         while(true){
             int a=x.compareTo(temp.data);
             if(a>0){
                 if(temp.right==null){
-                    temp.right=new Node(x);
                     return ;
                 }
                 temp=temp.right;
@@ -57,60 +58,65 @@ public class BST<T extends Comparable<T>> {
                 temp=temp.left;
             }
             else {
-                temp.data=x;
+                temp.data=f;
                 return;
             }
         }
     }
-    public Node search(T x){
+    public Integer search(T x){
         Node temp=this.root;
         while(true){
             int a=x.compareTo(temp.data);
             if(a>0){
                 if(temp.right==null){
-                    return new Node();
+                    break;
                 }
                 temp=temp.right;
             } else if(a<0) {
                 if(temp.left==null){ 
-                    return new Node();
+                    break;
                 }
                 temp=temp.left;
             }
             else {
-                return temp;
+                return temp.idx;
             }
         }
+        return -1;
     }
     public void delete(T x){
         Node temp=this.root;
+        Node prev=null;
         while(true){
             int a=x.compareTo(temp.data);
             if(a>0){
                 if(temp.right==null){
                     return ;
                 }
+                prev=temp;
                 temp=temp.right;
             } else if(a<0) {
                 if(temp.left==null){ 
                     return ;
                 }
+                prev=temp;
                 temp=temp.left;
             }
             else {
-                temp=null;
+                if(prev==null) this.root=null;
+                else if(prev.left.data==x) prev.left=null;
+                else prev.right=null;
                 return ;
             }
         }
     }
     private void makeTree(T[] arr){
-        for(T x:arr) insert(x);
+        for(int i=0;i<arr.length;++i) insert(arr[i],i);
     }
     private void traverse(Node rt){
         if(rt==null) return;
         traverse(rt.left);
-        System.out.println(rt.data);
+        System.out.print(rt.data+" ");
         traverse(rt.right);
     }
-    
 }
