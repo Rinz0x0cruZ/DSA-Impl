d9731ba0eac045a7d1a690b17af30f89fa33ae74
Rinz0x0cruZ <rinz0x0cruz@gmail.com>
2024-01-04 22:29:59 +0530

    AVL

--- DataStructures/AVLTree/AVL.java
+++ DataStructures/AVLTree/AVL.java
@@ -1,67 +1,125 @@
-public class AVL<T extends Comparable<T>>{
+public class AVL {
     class Node{
+        Integer data,height;
         Node left,right;
-        T data;
-        Integer height;
-        Node(T data){
+        Node(Integer data){
             this.data=data;
             this.height=0;
+            this.left=null;
+            this.right=null;
         }
     }
     Node root;
-    private void finder(Node temp,T x){
+    public AVL(Integer[] arr){
+        makeTree(arr);
+        Node temp=this.root;
+        setHeight(temp);
+    }
+
+    private void insert(Integer d){
+        if(this.root==null){ 
+            this.root=new Node(d);
+            return ;
+        }
+        Node temp=this.root;
         while(true){
-            if(x.compareTo(temp.data)<=0){ 
-                if(temp.left==null){ 
-                    temp.left=new Node(x);
-                    break;
+            Integer rootval=temp.data;
+            if(d<=rootval){
+                if(temp.left==null){
+                    temp.left=new Node(d);
+                    return ;
                 }
                 temp=temp.left;
             }
-            else{ 
+            else{
                 if(temp.right==null){
-                    temp.right=new Node(x);
-                    break;
+                    temp.right=new Node(d);
+                    return ;
                 }
                 temp=temp.right;
             }
         }
     }
-    private void rotateLeft(Node temp){
 
-    }
-    private void rotateRight(Node temp){
+    public Integer setHeight(Node node){
+        if(node==null) return 0;
 
-    }
-    private int setHeight(Node temp){
-        if(temp==null) return 0;
-        int left=setHeight(temp.left);
-        int right=setHeight(temp.right);
-        temp.height=1+(left>right?left:right);
-        return temp.height;
-    }
-    public void makeTree(T[] arr){
-        this.root=new Node(arr[0]);
-        Node temp;
-        int n=arr.length;
-        for(int i=1;i<n;++i){
-            temp=this.root;
-            T x=arr[i];
-            finder(temp,x);
-        }
-        temp=this.root;
-        setHeight(temp);
+        Integer left=setHeight(node.left);
+        Integer right=setHeight(node.right);
+
+        node.height=1+(left>right ? left : right);
+
+        return node.height;
     }
 
-    public void insert(T data){
+    public Integer getBalance(Node node){
+        if(node==null) return 0;
+        Integer left=(node.left==null ? 0 : node.left.height);
+        Integer right=(node.right==null ? 0 : node.right.height);
+        return left-right;
+    }
+    public void rotateCheck(){
+        Node fault=null;
         Node temp=this.root;
-        finder(temp,data);
+        
+        while(true){
+            Integer balance=getBalance(temp);
+            if(balance==0 || balance==1 || balance==-1) break;
+            else if(balance==-2 || balance==2) fault=temp;
+            else{
+                if(balance>0) temp=temp.right;
+                else temp=temp.left;
+            }
+        }
+
+        String rot="";
+        if(fault.left==null){ 
+            rot+='r';
+            temp=fault.right;
+        }
+        else{
+            rot+='l';
+            temp=fault.left;
+        }
+        if(temp.left==null) rot+='r';
+        else rot+='l';
+        
+        if(rot=="rr") r_r(fault);
+        else if(rot=="ll") l_l(fault);
+        else if(rot=="lr") l_r(fault);
+        else r_l(fault);
     }
 
-    public void display(Node root){
-        if(root==null) return;
-        display(root.left);
-        System.out.println(root.data+" "+root.height);
-        display(root.right);
+    public void r_r(Node node){
+        Node t=node;
+        node=node.right;
+        node.left=t;
+    }
+
+    public void l_l(Node node){
+
+    }
+
+    public void l_r(Node node){
+
+    }
+
+    public void r_l(Node node){
+
+    }
+    private void traverse(Node node){
+        if(node==null) return ;
+        traverse(node.left);
+        System.out.println(node.data);
+        traverse(node.right);
+    }
+
+    public void display(){
+        Node temp=this.root;
+        traverse(temp);
+    }
+
+    public void makeTree(Integer[] arr){
+        for(Integer x:arr) insert(x);
     }
 }
